3542435011d6e3bd2fc4d57a5acf7d8b7329c2d6
BartoszY <bartosz@yprojects.pl>
2021-01-15 11:31:05 +0100

    gulp config change to prod

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,7 @@ gulp.task('js', function() {
     .pipe( rename({ extname: '.min.js' }) )
     .pipe( buffer() )
     .pipe( sourcemaps.init({ loadMpas: true }) )
-    // .pipe( uglify() )
+    .pipe( uglify() )
     .pipe( sourcemaps.write('./') )
     .pipe( gulp.dest(path.prod.js) )
 });
